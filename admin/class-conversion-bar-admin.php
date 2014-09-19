7e1b722cd58612ea2c0398de070a93005f091c59
Fikri Rasyid <fikrirasyid@gmail.com>
2014-09-19 16:14:13 +0700

    Displaying conversion admin UI
    - Displaying conversion bar message textarea
    - Removing all unwanted meta box

--- admin/class-conversion-bar-admin.php
+++ admin/class-conversion-bar-admin.php
@@ -141,7 +141,85 @@ class Conversion_Bar_Admin {
 			'exclude_from_search'=> true
 		);
 
-		register_post_type( 'conversion-bar', $args );		
+		register_post_type( 'conversion_bar', $args );		
 	}
 
+	/**
+	 * Add and manage metaboxes for conversion bar edit screen
+	 * 
+	 * @since 	1.0.0
+	 * @access 	public
+	 */
+	public function add_meta_boxes( $post ){
+		global $wp_meta_boxes;
+
+		// Before adding any meta boxes, remove any third party meta box first
+		$page 		= get_current_screen();
+		$page_id 	= $page->id;
+
+		// Loop the metaboxes and unset everything except the core metabox
+		if( isset( $wp_meta_boxes[$page_id] ) && is_array( $wp_meta_boxes[$page_id] ) ){
+			foreach ($wp_meta_boxes[$page_id] as $meta_boxes_key => $meta_boxes ) {
+				foreach ( $meta_boxes as $meta_box_key => $meta_box ) {
+
+					// Unset all meta box except the core
+					if( 'core' != $meta_box_key ){
+						unset( $wp_meta_boxes[$page_id][$meta_boxes_key][$meta_box_key] );
+					}
+				}
+
+			}		
+		}
+
+		// Registering conversion bar's meta box
+	}
+
+	/**
+	 * Display conversion bar input below the title box
+	 * 
+	 * @access public
+	 * 
+	 * @since 1.0.0
+	 */
+	public function build_conversion_bar_input( $post ){
+		/**
+		 * Bail if this isn't admin screen
+		 */
+		if( !is_admin() ){
+			return;
+		}
+
+		/**
+		 * Bail if this isn't conversion bar screen
+		 */
+		if( !isset( $post->post_type ) || 'conversion_bar' != $post->post_type ){
+			return;
+		}
+
+		/**
+		 * Define post ID. This should be non-negative integer
+		 */
+		$post_id = ( int ) absint( $post->ID );
+
+		/**
+		 * Get current conversion-bar message
+		 */
+		$conversion_bar_message = get_post_meta( $post_id, '_conversion_bar_message', true );
+
+		/**
+		 * Create nonce field
+		 */
+		wp_nonce_field( "conversion_bar_{$post_id}", "_conversion_bar_nonce" );
+		?>
+		
+		<div id="conversion-bar-div">
+			<div id="conversion-bar-wrap">
+				<label for="conversion-bar-text" id="conversion-bar-label"><?php _e( 'Conversion Bar Message:', 'conversion-bar' ); ?></label>
+			<textarea type="text" name="conversion-bar-text" id="conversion-bar-text" placeholder="<?php _e( 'Type the message on your conversion bar (thing that will stick on the top of your browser) here', 'conversion bar' ); ?>"><?php echo esc_attr( htmlspecialchars( $conversion_bar_message ) ); ?></textarea>
+			</div>
+
+			<label for="content"><?php _e( 'Popup Message:' ); ?></label>
+		</div>
+		<?php
+	}
 }
--- includes/class-conversion-bar.php
+++ includes/class-conversion-bar.php
@@ -153,6 +153,8 @@ class Conversion_Bar {
 		$plugin_admin = new Conversion_Bar_Admin( $this->get_conversion_bar(), $this->get_version() );
 
 		$this->loader->add_action( 'init', $plugin_admin, 'register_post_type' );		
+		$this->loader->add_action( 'add_meta_boxes_conversion_bar', $plugin_admin, 'add_meta_boxes' );		
+		$this->loader->add_action( 'edit_form_after_title', $plugin_admin, 'build_conversion_bar_input' );		
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
 
